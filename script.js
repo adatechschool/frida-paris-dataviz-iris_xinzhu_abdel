25307d906a62629f2787edb9be4d9df995fcbb13
Xinzhu99 <jxinzhu@gmail.com>
2025-07-22 16:27:36 +0200

    affichage dynamique testé fonctionnel

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+
+const form = document.querySelector("#form");
+const input = document.querySelector("#userInput");
+const cocktailPage = document.querySelector("#cocktailPage");
+
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const inputValue = input.value;
+    fetchCocktailName(inputValue);
+});
+
+const fetchCocktailName = async (value) => {
+    try {
+        const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`);
+        const data = await res.json();
+        // console.log("data",data);
+
+        for (const item of data.drinks){
+        cocktailPage.innerHTML += `<li>${item.strInstructionsFR}</li>`;
+        };
+    }
+    catch (error){
+        console.log("error", error);
+    };
+};
