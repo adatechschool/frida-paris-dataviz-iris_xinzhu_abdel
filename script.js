1571cfae8e1421aacd098e3e9c397c00a4b328c4
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-07-24 14:23:42 +0200

    change hidden

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ const toggle = document.querySelector("#menu-toggle")
 const menu = document.querySelector("#nav-menu")
 
 toggle.addEventListener("click", () => {
-    menu.classList.toggle("nav-menu")
+    menu.classList.toggle("hidden")
 })
 
 
