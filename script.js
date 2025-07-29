c7b5ba70ac8f1d4ed2ab12427356c71c251ab63c
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-07-29 11:59:45 +0200

    script récuperer et modifié version abdel

--- script.js
+++ script.js
@@ -299,47 +299,48 @@ const showAbout = () =>{
   </ul>`
 }
 
-const showMenuFrida = () =>{
-
+const showMenuFrida = () => {
   fridaCocktails.forEach(element => {
-    const divImg = document.createElement("div")
-    divImg.classList.add("imgCocktail")
-    menuFrida.appendChild(divImg)
-  
-    const image = document.createElement("img")
-    image.classList.add("cocktailImage")
-    divImg.appendChild(image)
-    image.src = element.image
-  
-    const divTxt = document.createElement("div")
-    divTxt.classList.add("textContainer")
-    menuFrida.appendChild(divTxt)
+    
+    const cocktailPage = document.createElement("div");
+    cocktailPage.classList.add("eachResultat"); 
+    menuFrida.appendChild(cocktailPage);
 
-    const h2 = document.createElement("h2")
-    divTxt.appendChild(h2)
-    h2.classList.add("cocktailName")
-    h2.innerHTML= element.title
+    const divImg = document.createElement("div");
+    divImg.classList.add("imgCocktail");
+    cocktailPage.appendChild(divImg);
 
-    const p = document.createElement("p")
-    divTxt.appendChild(p)
-    p.innerHTML=element.description
+    const image = document.createElement("img");
+    image.classList.add("cocktailImage");
+    image.src = element.image;
+    image.alt = element.title;
+    divImg.appendChild(image);
 
-    const ul = document.createElement("ul")
-    ul.classList.add("ingredientList")
-    divTxt.appendChild(ul)
+    const divTxt = document.createElement("div");
+    divTxt.classList.add("textContainer");
+    divImg.appendChild(divTxt);
+
+    const h2 = document.createElement("h2");
+    h2.classList.add("cocktailName");
+    h2.innerHTML = element.title;
+    divTxt.appendChild(h2);
+
+    const p = document.createElement("p");
+    p.innerHTML = element.description;
+    divTxt.appendChild(p);
+
+    const ul = document.createElement("ul");
+    ul.classList.add("ingredientList");
 
     element.ingredients.forEach(ing => {
       const li = document.createElement("li");
       li.textContent = `${ing.name} : ${ing.measure}`;
       ul.appendChild(li);
     });
-
-  
+    divTxt.appendChild(ul);
   });
+};
 
-
-
-}
 
 
 
