505517ad962d4820a29d2cda6f32870898e38d7f
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-07-30 12:39:34 +0200

    debug menu  et accueil

--- menuHamburger.js
+++ menuHamburger.js
@@ -8,16 +8,18 @@ export const menuHamburger = (choices) => {
     const aboutUs = document.querySelector("#aboutUs");
     const cocktailContainer = document.querySelector("#cocktailContainer");
     const homePage = document.querySelector("#homePage");
+    const form = document.querySelector("#form");
 
     choices.addEventListener("click", (event) => {
         event.preventDefault();
 
         const clickedText = event.target.innerHTML;
         if (clickedText === "Homepage") {
-            homePage.style.display="block";
+            // homePage.style.display="block";
             menuFrida.style.display="none";
             aboutUs.style.display="none";
-            cocktailContainer.style.display="none";
+            form.style.display="flex";
+            cocktailContainer.innerHTML= ""
         };
         if (clickedText === "Menu Frida") {
             menuFrida.style.display="block"
